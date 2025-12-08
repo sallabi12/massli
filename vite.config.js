603c4366725a4686a8fbaf0d3f4c145d996a69aa
sallabi <sallabiabdo2004@gmail.com>
2025-12-08 17:18:18 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/',   // because the site is at https://maasli.ai
+  base: '/massli/',   // because the site is at https://maasli.ai
   plugins: [
     vue(),
   ],
