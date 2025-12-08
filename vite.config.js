ee1408a4e47371b7b58c4b0dbf8b6662f894d5b7
sallabi <sallabiabdo2004@gmail.com>
2025-12-08 17:38:13 +0300

    final

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/massli/',   // IMPORTANT for GitHub Pages
+  base: '/',   // IMPORTANT for GitHub Pages
   plugins: [
     vue(),
   ],
